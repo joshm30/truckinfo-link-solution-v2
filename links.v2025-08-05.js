00342e87496bef4d150dfe8f3d84427325b534f4
Josh Meth <joshmeth30@gmail.com>
2025-08-12 17:57:20 -0700

    organied hotshot links

--- links.v2025-08-05.js
+++ links.v2025-08-05.js
@@ -1,6 +1,8 @@
 // links.v2025-08-05.js  (version by date or Git commit hash)
 window.LINK_DATA = [
+  // HOTSHOT INSURANCE
   // https://www.truckinfo.net/guide/hot-shot-insurance
+  // TOP TABLE
   {
     "Key": "hot-shot-hartford-table-position-1",
     "ID": "AI-hartford-table",
@@ -30,5 +32,36 @@ window.LINK_DATA = [
     "Class": "button cta slim web-lead w-button",
     "Target": "_blank",
     "Rel": "nofollow noopener noreferrer"
-  }
+  },
+  // BOTTOM TABLE
+  {
+    "Key": "hot-shot-hartford-table-bottom-position-1",
+    "ID": "AI-hartford-table",
+    "Href": "/angle-insurance-quote-hot-shot?kw=company-table-hartford",
+    "DataGATrack": "button-click,table,northland",
+    "Text": "Get Quote",
+    "Class": "button cta slim web-lead w-button",
+    "Target": "_blank",
+    "Rel": "nofollow noopener noreferrer"
+  },
+  {
+    "Key": "hot-shot-biberk-table-bottom-position-2",
+    "ID": "CINET-biberk-table",
+    "Href": "https://tivly.com/truckinfo-biberk?sr=hedgebb&kw1=TruckInfo-hot-shot-insurance&kw2=company-table-biberk",
+    "DataGATrack": "button-click,table,OOIDA",
+    "Text": "Get Quote",
+    "Class": "button cta slim web-lead w-button",
+    "Target": "_blank",
+    "Rel": "nofollow noopener noreferrer"
+  },
+  {
+    "Key": "hot-shot-progressive-table-bottom-position-3",
+    "ID": "btn-progressive-quote",
+    "Href": "/angle-insurance-quote?kw=progressive",
+    "DataGATrack": "button-click,table,progressive",
+    "Text": "Get Quote",
+    "Class": "button cta slim web-lead w-button",
+    "Target": "_blank",
+    "Rel": "nofollow noopener noreferrer"
+  },
 ];
