b3caf87534891f1e22c89454f2bb87664d8422b9
Josh Meth <joshmeth30@gmail.com>
2025-08-25 17:31:34 -0700

    added commercial truck insurance buttons

--- links.js
+++ links.js
@@ -1,7 +1,7 @@
 // MIXED
 
 window.LINK_DATA = [
-  // HOTSHOT INSURANCE
+  // *** HOTSHOT INSURANCE ***
   // https://www.truckinfo.net/guide/hot-shot-insurance
   // TOP TABLE
   
@@ -42,9 +42,109 @@ window.LINK_DATA = [
     "Href": "/insurance-quote-main?kw=hotshot&kw1=company-table-progressive&kw2=position-bottom",
     "DataGATrack": "button-click,table,northland"
   },
+  
+  // *** Commercial Truck Insurance ***
+  // https://www.truckinfo.net/guide/commercial-truck-insurance
+  // TOP TABLE
+  {
+    "Key": "commercial-truck-insurance-progressive-table-position-1",
+    "ID": "AI-progressive-table",
+    "Href": "/insurance-quote-main?kw=commercial-truck-insurance&kw1=company-table-progressive&kw2=position-top",
+    "DataGATrack": "button-click,table,northland"
+  },
+  {
+    "Key": "commercial-truck-insurance-biberk-table-position-2",
+    "ID": "CINET-biberk-table",
+    "Href": "https://tivly.com/truckinfo-biberk?sr=hedgebb&kw1=TruckInfo-commercial-truck-insurance&kw2=company-table-biberk",
+    "DataGATrack": "button-click,table,northland"
+  },
+  {
+    "Key": "commercial-truck-insurance-hartford-table-position-3",
+    "ID": "CINET-hartford-table",
+    "Href": "https://tivly.com/truckinfo-the-hartford-web?sr=hedgeh&kw1=TruckInfo-commercial-truck-insurance&kw2=company-table-hartford",
+    "DataGATrack": "button-click,table,northland"
+  },
+  // BOTTOM REVIEW TABLE
+  {
+    "Key": "commercial-truck-insurance-progressive-review-position-1",
+    "ID": "AI-progressive-table",
+    "Href": "/insurance-quote-main?kw=commercial-truck-insurance&kw1=company-review-progressive&kw2=position-1",
+    "DataGATrack": "button-click,company-review,progressive",
+    "Class": "button cta w-button"
+  },
+  {
+    "Key": "commercial-truck-insurance-OOIDA-review-position-2",
+    "ID": "CINET-OOIDA-review",
+    "Href": "https://tivly.com/truckinfo-ooida?sr=hedge&kw1=TruckInfo-commercial-truck-insurance&kw2=company-review-OOIDA",
+    "DataGATrack": "button-click,company-review,progressive",
+    "Class": "button cta w-button"
+  },
+  {
+    "Key": "commercial-truck-insurance-hartford-review-position-3",
+    "ID": "CINET-hartford-review",
+    "Href": "https://tivly.com/truckinfo-the-hartford-web?sr=hedgeh&kw1=TruckInfo-commercial-truck-insurance&kw2=company-review-hartford",
+    "DataGATrack": "button-click,company-review,progressive",
+    "Class": "button cta w-button"
+  },
+  {
+    "Key": "commercial-truck-insurance-ODD-review-position-4",
+    "ID": "ODD-AI-review",
+    "Href": "/angle-insurance-quote?kw=company-table-progressive",
+    "DataGATrack": "button-click,company-review,progressive",
+    "Class": "button cta w-button"
+  },
+  {
+    "Key": "commercial-truck-insurance-great-west-review-position-5",
+    "ID": "great-west-AI-review",
+    "Href": "/angle-insurance-quote?kw=company-table-progressive",
+    "DataGATrack": "button-click,company-review,progressive",
+    "Class": "button cta w-button"
+  },
+  {
+    "Key": "commercial-truck-insurance-northland-review-position-5",
+    "ID": "northland-AI-review",
+    "Href": "/angle-insurance-quote?kw=company-table-progressive",
+    "DataGATrack": "button-click,company-review,progressive",
+    "Class": "button cta w-button"
+  },
+  {
+    "Key": "commercial-truck-insurance-sentry-review-position-6",
+    "ID": "sentry-AI-review",
+    "Href": "/angle-insurance-quote?kw=company-table-progressive",
+    "DataGATrack": "button-click,company-review,progressive",
+    "Class": "button cta w-button"
+  },
+  {
+    "Key": "commercial-truck-insurance-geico-review-position-7",
+    "ID": "geico-AI-review",
+    "Href": "/angle-insurance-quote?kw=company-table-progressive",
+    "DataGATrack": "button-click,company-review,progressive",
+    "Class": "button cta w-button"
+  },
+  {
+    "Key": "commercial-truck-insurance-NITIC-review-position-8",
+    "ID": "CINET-NITIC-review",
+    "Href": "https://tivly.com/truckinfo-nitc?sr=hedge&kw1=TruckInfo-commercial-truck-insurance&kw2=company-review-NITIC",
+    "DataGATrack": "button-click,company-review,progressive",
+    "Class": "button cta w-button"
+  },
+  {
+    "Key": "commercial-truck-insurance-state-farm-review-position-9",
+    "ID": "state-farm-AI-review",
+    "Href": "/angle-insurance-quote?kw=company-table-progressive",
+    "DataGATrack": "button-click,company-review,progressive",
+    "Class": "button cta w-button"
+  },
+  {
+    "Key": "commercial-truck-insurance-biberk-review-position-10",
+    "ID": "CINET-biberk-review",
+    "Href": "https://tivly.com/truckinfo-biberk?sr=hedgebb&kw1=TruckInfo-commercial-truck-insurance&kw2=company-review-biberk",
+    "DataGATrack": "button-click,company-review,progressive",
+    "Class": "button cta w-button"
+  },
+  
 
-
-  // TEST PAGE BUTTONS
+  // *** TEST PAGE BUTTONS ***
   // https://www.truckinfo.net/commercial-truck-insurance-old
   {
     "Key": "hot-shot-hartford-table-position-1-test",
--- links/links-off.js
+++ links/links-off.js
@@ -42,6 +42,116 @@ window.LINK_DATA = [
     "Href": "https://tivly.com/commercial-truck-insurance?sr=hedge&kw1=TruckInfo-hot-shot-insurance&kw2=company-table-Progressive",
     "DataGATrack": "button-click,table,progressive"
   },
+
+  // Commercial Truck Insurance
+  // https://www.truckinfo.net/guide/commercial-truck-insurance
+  // TOP TABLE
+  {
+    "Key": "commercial-truck-insurance-progressive-table-position-1",
+    "ID": "progressive-table",
+    "Href": "https://www.progressivecommercial.com/lp/commercial-truck-insurance/?phonenumber=8552803955&code=8483300001&utm_medium=referral&utm_source=truckinfo&kw=table",
+    "DataGATrack": "button-click,table,progressive"
+  },
+  {
+    "Key": "commercial-truck-insurance-biberk-table-position-2",
+    "ID": "CINET-biberk-table",
+    "Href": "https://tivly.com/truckinfo-biberk?sr=hedgebb&kw1=TruckInfo-commercial-truck-insurance&kw2=company-table-biberk",
+    "DataGATrack": "button-click,table,northland"
+  },
+  {
+    "Key": "commercial-truck-insurance-hartford-table-position-3",
+    "ID": "CINET-hartford-table",
+    "Href": "https://tivly.com/truckinfo-the-hartford-web?sr=hedgeh&kw1=TruckInfo-commercial-truck-insurance&kw2=company-table-hartford",
+    "DataGATrack": "button-click,table,northland"
+  },
+  // BOTTOM REVIEW TABLE
+  {
+    "Key": "commercial-truck-insurance-progressive-review-position-1",
+    "ID": "progressive-review",
+    "Href": "https://www.progressivecommercial.com/lp/commercial-truck-insurance/?phonenumber=8552803955&code=8483300001&utm_medium=referral&utm_source=truckinfo&kw=review",
+    "DataGATrack": "button-click,company-review,progressive",
+    "Class": "button cta w-button"
+  },
+  {
+    "Key": "commercial-truck-insurance-OOIDA-review-position-2",
+    "ID": "CINET-OOIDA-review",
+    "Href": "https://tivly.com/truckinfo-ooida?sr=hedge&kw1=TruckInfo-commercial-truck-insurance&kw2=company-review-OOIDA",
+    "DataGATrack": "button-click,company-review,progressive",
+    "Class": "button cta w-button"
+  },
+  {
+    "Key": "commercial-truck-insurance-hartford-review-position-3",
+    "ID": "CINET-hartford-review",
+    "Href": "https://tivly.com/truckinfo-the-hartford-web?sr=hedgeh&kw1=TruckInfo-commercial-truck-insurance&kw2=company-review-hartford",
+    "DataGATrack": "button-click,company-review,progressive",
+    "Class": "button cta w-button"
+  },
+  {
+    "Key": "commercial-truck-insurance-ODD-review-position-4",
+    "ID": "ODD-review",
+    "Href": "https://www.owneroperatordirect.com/&kw=review",
+    "DataGATrack": "button-click,company-review,progressive",
+    "Class": "button cta w-button"
+  },
+  {
+    "Key": "commercial-truck-insurance-great-west-review-position-5",
+    "ID": "great-west-review",
+    "Href": "https://www.gwccnet.com/&kw=review",
+    "DataGATrack": "button-click,company-review,progressive",
+    "Class": "button cta w-button"
+  },
+  {
+    "Key": "commercial-truck-insurance-northland-review-position-5",
+    "ID": "northland-review",
+    "Href": "https://www.northlandins.com/&kw=review",
+    "DataGATrack": "button-click,company-review,progressive",
+    "Class": "button cta w-button"
+  },
+  {
+    "Key": "commercial-truck-insurance-sentry-review-position-6",
+    "ID": "sentry-review",
+    "Href": "https://www.sentry.com/who-we-serve/trucking-insurance?kw=company-review-sentry",
+    "DataGATrack": "button-click,company-review,progressive",
+    "Class": "button cta w-button"
+  },
+  {
+    "Key": "commercial-truck-insurance-geico-review-position-7",
+    "ID": "geico-review",
+    "Href": "https://www.geico.com/commercial-auto-insurance/?kw=company-review-geico",
+    "DataGATrack": "button-click,company-review,progressive",
+    "Class": "button cta w-button"
+  },
+  {
+    "Key": "commercial-truck-insurance-NITIC-review-position-8",
+    "ID": "CINET-NITIC-review",
+    "Href": "https://tivly.com/truckinfo-nitc?sr=hedge&kw1=TruckInfo-commercial-truck-insurance&kw2=company-review-NITIC",
+    "DataGATrack": "button-click,company-review,progressive",
+    "Class": "button cta w-button"
+  },
+  {
+    "Key": "commercial-truck-insurance-state-farm-review-position-9",
+    "ID": "state-farm-review",
+    "Href": "https://www.statefarm.com/small-business-solutions/insurance/business-owners-policies/distributor-insurance?kw=company-review-state-farm",
+    "DataGATrack": "button-click,company-review,progressive",
+    "Class": "button cta w-button"
+  },
+  {
+    "Key": "commercial-truck-insurance-biberk-review-position-10",
+    "ID": "CINET-biberk-review",
+    "Href": "https://tivly.com/truckinfo-biberk?sr=hedgebb&kw1=TruckInfo-commercial-truck-insurance&kw2=company-review-biberk",
+    "DataGATrack": "button-click,company-review,progressive",
+    "Class": "button cta w-button"
+  },
+
+  // TEST PAGE BUTTONS
+  // https://www.truckinfo.net/commercial-truck-insurance-old
+  {
+    "Key": "hot-shot-hartford-table-position-1-test",
+    "ID": "AI-hartford-table",
+    "Href": "/insurance-quote-main?kw=hotshot&kw1=company-table-hartford&kw2=position-top",
+    "DataGATrack": "button-click,table,northland",
+    "Class": "button cta w-button test-class"
+  },
 ];
 
 // Example: <a data-insurance-link="hot-shot-hartford-table-position-1" class="button w-button">Get Quote</a>
